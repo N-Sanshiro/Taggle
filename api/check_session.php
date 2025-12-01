71b0d75b9d62f4ed8d58a6b3dc8998646c653476
N-Sanshiro <1227068@st.anan-nct.ac.jp>
2025-12-01 20:33:46 +0900

    修正

--- api/check_session.php
+++ api/check_session.php
@@ -5,6 +5,6 @@ session_start();
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode([
   'ok'        => true,
-  'logged_in' => isset($_SESSION['id_user']),
-  'id_user'   => $_SESSION['id_user'] ?? null,
+  'logged_in' => isset($_SESSION['uid']),
+  'id_user'   => $_SESSION['uid'] ?? null,
 ], JSON_UNESCAPED_UNICODE);
